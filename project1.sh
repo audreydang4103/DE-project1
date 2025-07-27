39338df265da312c137d599f18921581770c0712
Audrey Dang <audreydang4103@gmail.com>
2025-07-26 22:18:37 -0500

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -9,15 +9,15 @@ csvsql --query "SELECT * FROM movies WHERE vote_average > 7.5" --tables movies s
 #3
 
 #Min
-csvsort -c revenue tmdb-movies.csv | head -n 2 | csvcut -c id,original_title,revenue_adj
+csvsort -c revenue tmdb-movies.csv | head -n 2 | csvcut -c id,original_title,revenue
 #Max:
-csvsort -c revenue -r tmdb-movies.csv | head -n 2 | csvcut -c id,original_title,revenue_adj
+csvsort -c revenue -r tmdb-movies.csv | head -n 2 | csvcut -c id,original_title,revenue
 
 #4
 csvcut -c revenue tmdb-movies.csv | csvstat --sum
 
 #5 
-csvsort -c revenue -r tmdb-movies.csv | head -n 11 | csvcut -c id,original_title,revenue_adj
+csvsort -c revenue -r tmdb-movies.csv | head -n 11 | csvcut -c id,original_title,revenue
 
 #6
 csvcut -c director tmdb-movies.csv | csvstat --freq | tail -n 1 | jq -r 'to_entries | sort_by(.value) | .[-1] | "\(.key): \(.value)"'
